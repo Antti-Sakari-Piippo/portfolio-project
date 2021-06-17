f2a961f61a3ddc9c508188e39536a7dadd2412b5
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-17 13:28:08 +0300

    Prettier refactor

--- src/Components/ServicesSection.js
+++ src/Components/ServicesSection.js
@@ -1,15 +1,15 @@
 function ServicesSection({ image, title, text }) {
-  return (
-    <div className="ServicesSection">
-      <div className="service">
-        <div className="service-content">
-          <img src={image} alt="" />
-          <h5 className="s-title">{title}</h5>
-          <p className="s-text">{text}</p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="ServicesSection">
+			<div className="service">
+				<div className="service-content">
+					<img src={image} alt="" />
+					<h5 className="s-title">{title}</h5>
+					<p className="s-text">{text}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ServicesSection;
