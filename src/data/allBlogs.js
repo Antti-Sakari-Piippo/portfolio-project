d10f2514d10ec0070ecf60503fe0b83759ea6ef8
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-18 18:05:23 +0300

    Add text field to blog data

--- src/data/allBlogs.js
+++ src/data/allBlogs.js
@@ -1,60 +1,72 @@
 import blog from "../images/blogs/defaultBlog.png";
 
 const blogs = [
-  {
-    id: 1,
-    image: blog,
-    title: "Lorem ipsum dolor sit amet consectetur",
-    link: "https://www.facebook.com/",
-    date: "21",
-    month: "Mar",
-    year: "2021",
-  },
-  {
-    id: 2,
-    image: blog,
-    title: "Lorem ipsum dolor sit amet consectetur",
-    link: "https://www.facebook.com/",
-    date: "09",
-    month: "Dec",
-    year: "2021",
-  },
-  {
-    id: 3,
-    image: blog,
-    title: "Lorem ipsum dolor sit amet consectetur",
-    link: "https://www.facebook.com/",
-    date: "15",
-    month: "Jun",
-    year: "2019",
-  },
-  {
-    id: 4,
-    image: blog,
-    title: "Lorem ipsum dolor sit amet consectetur",
-    link: "https://www.facebook.com/",
-    date: "04",
-    month: "Aug",
-    year: "2020",
-  },
-  {
-    id: 5,
-    image: blog,
-    title: "Lorem ipsum dolor sit amet consectetur",
-    link: "https://www.facebook.com/",
-    date: "09",
-    month: "Sep",
-    year: "2020",
-  },
-  {
-    id: 6,
-    image: blog,
-    title: "Lorem ipsum dolor sit amet consectetur",
-    link: "https://www.facebook.com/",
-    date: "09",
-    month: "Sep",
-    year: "2020",
-  },
+	{
+		id: 1,
+		image: blog,
+		title: "First Title",
+		text:
+			"Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur",
+		link: "https://www.facebook.com/",
+		date: "21",
+		month: "Mar",
+		year: "2021"
+	},
+	{
+		id: 2,
+		image: blog,
+		title: "Second Title",
+		text:
+			"Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur",
+		link: "https://www.facebook.com/",
+		date: "09",
+		month: "Dec",
+		year: "2021"
+	},
+	{
+		id: 3,
+		image: blog,
+		title: "Third title",
+		text:
+			"Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur",
+		link: "https://www.facebook.com/",
+		date: "15",
+		month: "Jun",
+		year: "2019"
+	},
+	{
+		id: 4,
+		image: blog,
+		title: "Lorem ipsum dolor sit amet consectetur",
+		text:
+			"Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur",
+		link: "https://www.facebook.com/",
+		date: "04",
+		month: "Aug",
+		year: "2020"
+	},
+	{
+		id: 5,
+		image: blog,
+		title: "Lorem ipsum dolor sit amet consectetur",
+		text:
+			"Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur",
+		link: "https://www.facebook.com/",
+		date: "09",
+		month: "Sep",
+		year: "2020"
+	},
+	{
+		id: 6,
+		image: blog,
+		title: "Lorem ipsum dolor sit amet consectetur",
+		text:
+			"Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur",
+		link: "https://www.facebook.com/",
+		date: "09",
+		month: "Sep",
+		year: "2020"
+	}
 ];
 
 export default blogs;
