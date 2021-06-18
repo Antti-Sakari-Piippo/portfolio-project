a3668d359585be0ca2e6c04fdc91b08789878ee1
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-18 18:04:38 +0300

    Create own component for BlogItem

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,30 +1,23 @@
 import { useState } from "react";
-import { generatePath, Route, useHistory, useRouteMatch } from "react-router-dom";
+// import { generatePath, Route, useHistory, useRouteMatch } from "react-router-dom";
 import Tittle from "../Components/Tittle";
-import allBlogs from "../data/allBlogs";
+import { useParams, BrowserRouter as Router, Link, Switch } from "react-router-dom";
+import { BlogItem } from "../Components/BlogItem";
 
-function BlogsPage() {
-  return (
-    <>
-      <div className="b-title">
-        <Tittle title={"Recent Blogs"} span={"Recent Blogs"} />
-      </div>
-      <div className="BlogsPage">
-        {allBlogs.map((blog) => {
-          return (
-            <div className="blog" key={blog.id}>
-              <div className="blog-content">
-                <img src={blog.image} alt="" />
-                <a href={blog.link} className="blog-link">
-                  {blog.title}
-                </a>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+function BlogsPage({ correctBlogF, blogsArray, blogItem }) {
+	const { id } = useParams();
+	return (
+		<div>
+			<div className="b-title">
+				<Tittle title={"Recent Blogs"} span={"Recent Blogs"} />
+			</div>
+			<div className="BlogsPage">
+				{blogsArray.map((blog) => {
+					return <BlogItem onClick={() => correctBlogF(blogsArray)} blog={blog} blogItem={blog} />;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default BlogsPage;
