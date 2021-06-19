ded19b609314fb7a47d6177692357d548530648b
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-19 14:04:56 +0300

    Transfer useparams to Blogs page

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,14 +1,18 @@
 import { useParams } from "react-router-dom";
+import blogs from "../data/allBlogs";
 
-export const Blog = ({ blogItem }) => {
+export const Blog = () => {
+	let { slug } = useParams();
+	const blogsArray = [ ...blogs.map((item) => item) ];
+	const currentItem = blogsArray[slug - 1];
 	return (
 		<div className="blog-item">
-			<p className="blog-title">{blogItem.title}</p>
-			<img src={blogItem.image} />
-			<p className="blog-text">{blogItem.text}</p>
+			<p className="blog-title">{currentItem.title}</p>
+			<img src={currentItem.image} />
+			<p className="blog-text">{currentItem.text}</p>
 			<br />
 			<p>
-				{blogItem.date} {blogItem.month} {blogItem.year}
+				{currentItem.date} {currentItem.month} {currentItem.year}
 			</p>
 		</div>
 	);
