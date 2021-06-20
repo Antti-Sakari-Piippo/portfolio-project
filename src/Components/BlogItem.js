5ff013b78edeb96673d63e58b6b32f23d275c6e8
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-20 13:45:06 +0300

    Add excerpt function for blogItem texts

--- src/Components/BlogItem.js
+++ src/Components/BlogItem.js
@@ -1,12 +1,20 @@
 import { Link } from "react-router-dom";
 
 export const BlogItem = ({ blog }) => {
+	const excerpt = (text) => {
+		let trimmedString = text.substr(0, 100);
+		trimmedString =
+			trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + '..."';
+		return trimmedString;
+	};
+
 	return (
 		<div className="blogItem">
 			<div className="blog-content">
 				<Link to={`/blogs/${blog.id}`}>
 					<img src={blog.image} alt="blog" />
 					<p className="blog-link">{blog.title}</p>
+					<p className="excerpt">{excerpt(blog.text)}</p>
 				</Link>
 			</div>
 		</div>
