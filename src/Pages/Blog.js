4fe024dbe21f5a9a8af31f667efe50aaf96e8210
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-20 12:20:12 +0300

    Add back button

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,19 +1,35 @@
 import { useParams } from "react-router-dom";
+import { BlockList } from "../Components/BlockList";
 import blogs from "../data/allBlogs";
+import { Link } from "react-router-dom";
 
 export const Blog = () => {
 	let { slug } = useParams();
 	const blogsArray = [ ...blogs.map((item) => item) ];
 	const currentItem = blogsArray[slug - 1];
 	return (
-		<div className="blog-item">
+		<div>
 			<p className="blog-title">{currentItem.title}</p>
-			<img src={currentItem.image} />
-			<p className="blog-text">{currentItem.text}</p>
-			<br />
-			<p>
-				{currentItem.date} {currentItem.month} {currentItem.year}
-			</p>
+			<div className="blog-item">
+				<div className="wrapper-left">
+					<img src={currentItem.image} />
+					<p className="blog-text">{currentItem.text}</p>
+					<br />
+					<p className="blog-date">
+						{currentItem.date} {currentItem.month} {currentItem.year}
+					</p>
+					<br />
+					<Link to={`/blogs/`} className="button blog-link">
+						<i className="fas fa-angle-left" />
+						Back
+					</Link>
+				</div>
+				<div className="wrapper-right">
+					<div className="BlockList">
+						<BlockList />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
