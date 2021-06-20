a39b7b0f4af36c224f4bb98f760d8433e3bd3bd5
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-20 21:48:34 +0300

    Remove unused blogArray

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -3,14 +3,13 @@ import { BlogItem } from "../Components/BlogItem";
 import blogs from "../data/allBlogs";
 
 function BlogsPage() {
-	const blogsArray = [ ...blogs.map((item) => item) ];
 	return (
 		<div className="BlogsPage">
 			<div className="blogs-title">
 				<Tittle title={"Recent Blogs"} span={"Recent Blogs"} />
 			</div>
 			<div className="blogs">
-				{blogsArray.map((blog) => {
+				{blogs.map((blog) => {
 					return (
 						<div>
 							<BlogItem blog={blog} />
