7ba68206315b424b209b3242f0804cee0a2cb861
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-20 12:20:32 +0300

    Change skillSection texts

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -7,39 +7,39 @@ import intelligence from "../images/intelligence.svg";
 import gamedev from "../images/game-dev.svg";
 
 function AboutPage() {
-  return (
-    <div className="AboutPage">
-      <Tittle title={"About Me"} span={"About Me"} />
-      <ImageSection />
-      <Tittle title={"My Skills"} span={"My Skills"} />
-      <div className="skillsContainer">
-        <SkillsSection skill={"Javascript"} progress={"60%"} width={"60%"} />
-        <SkillsSection skill={"React Js"} progress={"65%"} width={"65%"} />
-        <SkillsSection skill={"CSS and SCSS"} progress={"80%"} width={"80%"} />
-        <SkillsSection skill={"Unity"} progress={"30%"} width={"30%"} />
-        <SkillsSection skill={"SQL"} progress={"60%"} width={"60%"} />
-      </div>
+	return (
+		<div className="AboutPage">
+			<Tittle title={"About Me"} span={"About Me"} />
+			<ImageSection />
+			<Tittle title={"My Skills"} span={"My Skills"} />
+			<div className="skillsContainer">
+				<SkillsSection skill={"Javascript"} progress={"60%"} width={"60%"} />
+				<SkillsSection skill={"React Js"} progress={"65%"} width={"65%"} />
+				<SkillsSection skill={"CSS and SCSS"} progress={"70%"} width={"70%"} />
+				<SkillsSection skill={"Python"} progress={"50%"} width={"50%"} />
+				<SkillsSection skill={"SQL"} progress={"60%"} width={"60%"} />
+			</div>
 
-      <Tittle title={"Services"} span={"Services"} />
-      <div className="services-container">
-        <ServicesSection
-          image={design}
-          title={"Web design"}
-          text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-        />
-        <ServicesSection
-          image={intelligence}
-          title={"Artificial Intelligence"}
-          text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-        />
-        <ServicesSection
-          image={gamedev}
-          title={"Game Development"}
-          text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-        />
-      </div>
-    </div>
-  );
+			<Tittle title={"Services"} span={"Services"} />
+			<div className="services-container">
+				<ServicesSection
+					image={design}
+					title={"Web design"}
+					text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+				/>
+				<ServicesSection
+					image={intelligence}
+					title={"Artificial Intelligence"}
+					text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+				/>
+				<ServicesSection
+					image={gamedev}
+					title={"Game Development"}
+					text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default AboutPage;
