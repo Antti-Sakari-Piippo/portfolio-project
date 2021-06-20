b57c03f6981c79f7ea07289da740e4f27bfbda00
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-20 21:50:20 +0300

    Remove unused blogArray

--- src/Components/BlockList.js
+++ src/Components/BlockList.js
@@ -2,12 +2,10 @@ import blogs from "../data/allBlogs";
 import { Link } from "react-router-dom";
 
 export const BlockList = () => {
-	const blogsArray = [ ...blogs.map((item) => item) ];
-
 	return (
 		<div className="blogList">
 			<h4>All blogs</h4>
-			{blogsArray.map((blog) => {
+			{blogs.map((blog) => {
 				return (
 					<Link to={`/blogs/${blog.id}`}>
 						<p className="blog-link">{blog.title}</p>
