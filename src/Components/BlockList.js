aaafb59f69bd0b8d0d3c73c4da3ae7959e8f5c8c
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-20 12:24:17 +0300

    Create BlogList page

---
+++ src/Components/BlockList.js
@@ -0,0 +1,20 @@
+import blogs from "../data/allBlogs";
+import { Link } from "react-router-dom";
+
+export const BlockList = () => {
+	const blogsArray = [ ...blogs.map((item) => item) ];
+	const cars = [ "BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi" ];
+
+	return (
+		<div className="blogList">
+			<h4>All blogs</h4>
+			{blogsArray.map((blog) => {
+				return (
+					<Link to={`/blogs/${blog.id}`}>
+						<p className="blog-link">{blog.title}</p>
+					</Link>
+				);
+			})}
+		</div>
+	);
+};
