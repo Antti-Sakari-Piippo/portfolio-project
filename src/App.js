cdaaf6614d55d9219d6425bc334957cdf213b75a
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-06-21 17:45:25 +0300

    Create dark and light mode for all pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import "./App.scss";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
-import { Switch, Route } from "react-router-dom";
+import { Switch as PageSwitchin, Route } from "react-router-dom";
 import AboutPage from "./Pages/AboutPage";
 import PortfoliosPage from "./Pages/PortfoliosPage";
 import BlogsPage from "./Pages/BlogsPage";
 import ContactPage from "./Pages/ContactPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Particles from "react-particles-js";
 import { Blog } from "./Pages/Blog";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Switch from "@material-ui/core/Switch";
 
 const App = () => {
 	const [ toggle, setToggle ] = useState(false);
+	const [ theme, setTheme ] = useState("dark-theme");
+	const [ checked, setChecked ] = useState(false);
+
+	useEffect(
+		() => {
+			document.documentElement.className = theme;
+		},
+		[ theme ]
+	);
+
+	const themeToggler = () => {
+		if (theme === "light-theme") {
+			setTheme("dark-theme");
+			setChecked(false);
+		} else {
+			setTheme("light-theme");
+			setChecked(true);
+		}
+	};
 
 	const navToggle = () => {
 		setToggle(!toggle);
@@ -26,26 +47,42 @@ const App = () => {
 				<div className="lines-1" />
 				<div className="lines-2" />
 				<div className="lines-3" />
+				<div className="theme">
+					<div className="light-dark-mode">
+						<div className="left-content">
+							<Brightness4Icon />
+						</div>
+						<div className="right-content">
+							<Switch
+								value=""
+								checked={checked}
+								inputProps={{ "aria-label": "" }}
+								size="medium"
+								onClick={themeToggler}
+							/>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div className="main-content">
 				<div className="content">
-					<Switch>
+					<PageSwitchin>
 						<Route path="/" exact>
 							<Particles
 								className="particles-canvas"
 								params={{
-									polygon: {
+									particles: {
 										number: {
-											value: 30,
+											value: 3,
 											density: {
 												enable: true,
-												value_area: 400
+												value_area: 1000
 											}
 										},
 										shape: {
 											type: "circle",
 											stroke: {
-												width: 10,
+												width: 1000,
 												color: "#E87A00"
 											}
 										}
@@ -63,17 +100,17 @@ const App = () => {
 						<Route path="/blogs" exact>
 							<BlogsPage />
 						</Route>
-					</Switch>
-					<Switch>
+					</PageSwitchin>
+					<PageSwitchin>
 						<Route exact path="/blogs/:slug">
 							<Blog />
 						</Route>
-					</Switch>
-					<Switch>
+					</PageSwitchin>
+					<PageSwitchin>
 						<Route path="/contact" exact>
 							<ContactPage />
 						</Route>
-					</Switch>
+					</PageSwitchin>
 				</div>
 			</div>
 		</div>
