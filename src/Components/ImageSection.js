e0e17f23ac5db70262531c4213857c5c601422d1
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-14 17:08:20 +0300

    Change info about me

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,39 +1,36 @@
 import blank from "../images/blank-profile-picture.webp";
 
 function ImageSection() {
-  return (
-    <div className="ImageSection">
-      <div className="img">
-        <img src={blank} alt="" />
-      </div>
-      <div className="about-info">
-        <h4>
-          I am<span> Lorem Ipsum</span>
-        </h4>
-        <p className="about-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          necessitatibus perferendis numquam dolorem cumque facilis. Desir per
-          po la ta doso vi slone ve?
-        </p>
-        <div className="about-details">
-          <div className="left-section">
-            <p>Full Name</p>
-            <p>Nationality</p>
-            <p>Age</p>
-            <p>Languages</p>
-            <p>Address</p>
-          </div>
-          <div className="right-section">
-            <p>Lorem Ipsum</p>
-            <p>Finnish</p>
-            <p>27</p>
-            <p>Finnish, English</p>
-            <p>Lorem ipsum 23</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="ImageSection">
+			<div className="img">
+				<img src={blank} alt="" />
+			</div>
+			<div className="about-info">
+				<h4>
+					I am<span> Antti Piippo</span>
+				</h4>
+				<p className="about-text">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum necessitatibus perferendis
+					numquam dolorem cumque facilis. Desir per po la ta doso vi slone ve?
+				</p>
+				<div className="about-details">
+					<div className="left-section">
+						<p>Full Name</p>
+						<p>Nationality</p>
+						<p>Age</p>
+						<p>Languages</p>
+					</div>
+					<div className="right-section">
+						<p>Antti Piippo</p>
+						<p>Finnish</p>
+						<p>28</p>
+						<p>Finnish, English</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ImageSection;
