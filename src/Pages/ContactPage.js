a7a170348511a8184359b092b74b22dea1cc7312
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-14 17:05:53 +0300

    Remove unused social icons

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -5,38 +5,28 @@ import location from "../images/location.svg";
 import Tittle from "../Components/Tittle";
 
 function ContactPage() {
-  return (
-    <div>
-      <div className="title">
-        <Tittle title={"About Me"} span={"About Me"} />
-      </div>
-      <div className="ContactPage">
-        <div className="map-sect">
-          <iframe
-            title="map"
-            width="500"
-            height="300"
-            src="https://api.maptiler.com/maps/streets/?key=ptRri5SRXyugsx5Bdomf#14.0/40.70850/-74.00603"
-          ></iframe>
-        </div>
-        <div className="contact-sect">
-          <ContactItem title={"Phone"} icon={phone} text1={"+123 123123123"} />
-          <ContactItem
-            title={"Email"}
-            icon={email}
-            text1={"loremimpsum@gmail.com"}
-            text2={"abcloremipsum@hotmail.com"}
-          />
-          <ContactItem
-            icon={location}
-            text1={"Lorem ipsum 25"}
-            text2={"Finland"}
-            title={"Address"}
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="title">
+				<Tittle title={"About Me"} span={"About Me"} />
+			</div>
+			<div className="ContactPage">
+				<div className="map-sect">
+					<iframe
+						title="map"
+						width="500"
+						height="300"
+						src="https://api.maptiler.com/maps/streets/?key=ptRri5SRXyugsx5Bdomf#14.0/40.70850/-74.00603"
+					/>
+				</div>
+				<div className="contact-sect">
+					<ContactItem title={"Phone"} icon={phone} text1={"045 2123490"} />
+					<ContactItem title={"Email"} icon={email} text1={"antti.sakari.piippo@gmail.com"} />
+					<ContactItem icon={location} title={"LinkedIn"} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ContactPage;
