0457a3c7cd06b4e051e5c6f0bf747f5fcc727254
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-15 17:27:47 +0300

    Change to use my real location in iframe map

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -13,16 +13,24 @@ function ContactPage() {
 			<div className="ContactPage">
 				<div className="map-sect">
 					<iframe
-						title="map"
 						width="500"
-						height="300"
-						src="https://api.maptiler.com/maps/streets/?key=ptRri5SRXyugsx5Bdomf#14.0/40.70850/-74.00603"
+						height="500"
+						src="https://maps.google.com/maps?q=H%C3%B6yhty%C3%A4,%20Oulu&t=&z=13&ie=UTF8&iwloc=&output=embed"
+						frameborder="0"
+						scrolling="no"
 					/>
 				</div>
 				<div className="contact-sect">
-					<ContactItem title={"Phone"} icon={phone} text1={"045 2123490"} />
 					<ContactItem title={"Email"} icon={email} text1={"antti.sakari.piippo@gmail.com"} />
-					<ContactItem icon={location} title={"LinkedIn"} />
+					<ContactItem title={"Phone"} icon={phone} text1={"045 2123490"} />
+					<ContactItem
+						icon={location}
+						title={
+							<a href="https://www.linkedin.com/in/piippo-antti" target="_blank">
+								LinkedIn
+							</a>
+						}
+					/>
 				</div>
 			</div>
 		</div>
