a322a7211f3cd210c4db6293114ead3a5da20158
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-15 16:49:00 +0300

    Remove unused component and its styles

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,6 @@
 import Tittle from "../Components/Tittle";
 import ImageSection from "../Components/ImageSection";
 import SkillsSection from "../Components/SkillsSection";
-import ServicesSection from "../Components/ServicesSection";
-import design from "../images/design.svg";
-import intelligence from "../images/intelligence.svg";
-import gamedev from "../images/game-dev.svg";
 
 function AboutPage() {
 	return (
@@ -16,27 +12,8 @@ function AboutPage() {
 				<SkillsSection skill={"React Js"} progress={"65%"} width={"65%"} />
 				<SkillsSection skill={"WordPress"} progress={"55%"} width={"55%"} />
 				<SkillsSection skill={"CSS and SCSS"} progress={"70%"} width={"70%"} />
-				<SkillsSection skill={"MySQL"} progress={"50%"} width={"50%"} />
+				<SkillsSection skill={"MySQL"} progress={"60%"} width={"60%"} />
 				<SkillsSection skill={"Data-analysis"} progress={"40%"} width={"40%"} />
-			</div>
-
-			<Tittle title={"Services"} span={"Services"} />
-			<div className="services-container">
-				<ServicesSection
-					image={design}
-					title={"Web design"}
-					text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-				/>
-				<ServicesSection
-					image={intelligence}
-					title={"Artificial Intelligence"}
-					text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-				/>
-				<ServicesSection
-					image={gamedev}
-					title={"Game Development"}
-					text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-				/>
 			</div>
 		</div>
 	);
