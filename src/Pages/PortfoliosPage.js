f1bfee37d3f8f235367bbc6cf3c76d460fa5262f
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-23 21:13:06 +0300

    Fix return statement

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -4,34 +4,33 @@ import Tittle from "../Components/Tittle";
 import portfolios from "../data/allportfolios";
 import { useState } from "react";
 
-const allCategories = ["All", ...portfolios.map((item) => item.category)];
+const allCategories = [ "All", ...portfolios.map((item) => item.category) ];
 
 function PortfoliosPage() {
-  const [categories, setCategories] = useState(allCategories);
-  const [menuItems, setMenuItems] = useState(portfolios);
+	const [ categories, setCategories ] = useState(allCategories);
+	const [ menuItems, setMenuItems ] = useState(portfolios);
 
-  const filter = (category) => {
-    if (category === "All") {
-      setMenuItems(portfolios);
-      return;
-    }
-    const filteredData = portfolios.filter((item) => {
-      return item.category === category;
-    });
-    setMenuItems(filteredData);
-  };
+	const filter = (category) => {
+		if (category === "All") {
+			return setMenuItems(portfolios);
+		}
+		const filteredData = portfolios.filter((item) => {
+			return item.category === category;
+		});
+		setMenuItems(filteredData);
+	};
 
-  return (
-    <div className="PortfolioPage">
-      <div className="title">
-        <Tittle title={"Portfolios"} span={"portfolios"} />
-      </div>
-      <div>
-        <Categories filter={filter} categories={categories} />
-        <MenuItems menuItems={menuItems} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="PortfolioPage">
+			<div className="title">
+				<Tittle title={"Portfolios"} span={"portfolios"} />
+			</div>
+			<div>
+				<Categories filter={filter} categories={categories} />
+				<MenuItems menuItems={menuItems} />
+			</div>
+		</div>
+	);
 }
 
 export default PortfoliosPage;
