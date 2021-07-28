685c7a55e0e92286da3272955dcbed789e1597f3
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-28 20:17:07 +0300

    Change block text

--- src/data/allBlogs.js
+++ src/data/allBlogs.js
@@ -6,7 +6,7 @@ const blogs = [
 		image: blog,
 		title: "First blog",
 		text:
-			'"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness..."',
+			'Wise words by Cicero: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness..."',
 		link: "https://www.facebook.com/",
 		date: "8",
 		month: "March",
