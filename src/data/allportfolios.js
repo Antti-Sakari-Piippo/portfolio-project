a4843b4e39c78305939612ee1c3ed01dc9b44e30
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2021-07-28 21:37:36 +0300

    Change portfolio projects order

--- src/data/allportfolios.js
+++ src/data/allportfolios.js
@@ -8,35 +8,15 @@ const portfolios = [
 	{
 		id: 1,
 		category: "React Js",
-		link1: "https://github.com/Antti-Sakari-Piippo/weather-app-react-hooks",
-		link2: "https://frosty-hopper-57a7b9.netlify.app",
+		link1: "https://github.com/Antti-Sakari-Piippo/redux-shopping",
+		link2: "https://eager-jennings-c0c61f.netlify.app",
 		icon1: <FontAwesomeIcon icon={faGithub} />,
 		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
 		image: port1,
-		title: "Weather App"
+		title: "Shopping cart [Redux]"
 	},
 	{
 		id: 2,
-		category: "CSS",
-		link1: "https://github.com/Antti-Sakari-Piippo/CSS-SHAPES",
-		link2: "https://peaceful-turing-1c48b2.netlify.app",
-		icon1: <FontAwesomeIcon icon={faGithub} />,
-		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
-		image: port1,
-		title: "HTML and SCSS shapes"
-	},
-	{
-		id: 3,
-		category: "React Js",
-		link1: "https://github.com/Antti-Sakari-Piippo/crypto-2",
-		link2: "https://practical-colden-9a6133.netlify.app",
-		icon1: <FontAwesomeIcon icon={faGithub} />,
-		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
-		image: port1,
-		title: "Crypto Market Cap"
-	},
-	{
-		id: 4,
 		category: "React Js",
 		link1: "https://github.com/Antti-Sakari-Piippo/portfolio-project",
 		link2: "", //https://modest-noyce-6b7122.netlify.app
@@ -46,17 +26,47 @@ const portfolios = [
 		title: "Portfolio App"
 	},
 	{
+		id: 3,
+		category: "React Js",
+		link1: "https://github.com/Antti-Sakari-Piippo/weather-app-react-hooks",
+		link2: "https://frosty-hopper-57a7b9.netlify.app",
+		icon1: <FontAwesomeIcon icon={faGithub} />,
+		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
+		image: port1,
+		title: "Weather App"
+	},
+	{
+		id: 4,
+		category: "CSS",
+		link1: "https://github.com/Antti-Sakari-Piippo/CSS-SHAPES",
+		link2: "https://peaceful-turing-1c48b2.netlify.app",
+		icon1: <FontAwesomeIcon icon={faGithub} />,
+		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
+		image: port1,
+		title: "HTML and SCSS shapes"
+	},
+	{
 		id: 5,
+		category: "React Js",
+		link1: "https://github.com/Antti-Sakari-Piippo/crypto-2",
+		link2: "https://practical-colden-9a6133.netlify.app",
+		icon1: <FontAwesomeIcon icon={faGithub} />,
+		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
+		image: port1,
+		title: "Crypto Market Cap"
+	},
+	{
+		id: 6,
 		category: "Python",
-		link1: "https://github.com/Antti-Sakari-Piippo",
-		link2: "https://github.com/Antti-Sakari-Piippo",
+		link1: "",
+		link2: "",
 		icon1: <FontAwesomeIcon icon={faGithub} />,
 		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
 		image: port1,
 		title: "Python (COMING SOON)"
 	},
 	{
-		id: 6,
+		id: 7,
 		category: "NodeJS",
 		link1: "https://github.com/Antti-Sakari-Piippo/bank-nodejs/blob/master/bank.js",
 		link2: "",
@@ -64,16 +74,6 @@ const portfolios = [
 		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
 		image: port1,
 		title: "Bank"
-	},
-	{
-		id: 7,
-		category: "Power BI",
-		link1: "",
-		link2: "",
-		icon1: <FontAwesomeIcon icon={faGithub} />,
-		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
-		image: port1,
-		title: "Tieliikenneonnettomuudet (COMING SOON)"
 	},
 	{
 		id: 8,
@@ -87,13 +87,13 @@ const portfolios = [
 	},
 	{
 		id: 9,
-		category: "React Js",
-		link1: "https://github.com/Antti-Sakari-Piippo/redux-shopping-card",
-		link2: "", //https://nervous-noyce-5d57e2.netlify.app  //FIX
+		category: "Power BI",
+		link1: "",
+		link2: "",
 		icon1: <FontAwesomeIcon icon={faGithub} />,
 		icon2: <Icon icon={netlifyIcon} width="80px" height="80px" />,
 		image: port1,
-		title: "Shopping cart [Redux]"
+		title: "Tieliikenneonnettomuudet (COMING SOON)"
 	}
 ];
 
