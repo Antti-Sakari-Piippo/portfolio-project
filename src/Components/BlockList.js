ea073897e7eb72fd8797047e4e2e93609eb65ac3
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2024-06-07 20:30:15 +0300

    Add work/education text

--- src/Components/BlockList.js
+++ src/Components/BlockList.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const BlockList = () => {
 	return (
 		<div className="blogList">
-			<h4>All blogs</h4>
+			<h4>All entries</h4>
 			{blogs.map((blog) => {
 				return (
 					<Link to={`/blogs/${blog.id}`}>
--- src/data/allBlogs.js
+++ src/data/allBlogs.js
@@ -4,18 +4,18 @@ const blogs = [
 	{
 		id: 1,
 		image: blog,
-		title: "Engineer(OAMK) Information technology",
+		title: "Engineer (OAMK) Information technology",
 		text:
-			'During my time at Oulu University of Applied Sciences (Oulun Ammattikorkeakoulu), where I earned a Bachelor of Information Technology, I developed a strong foundation in software development. My specialization in software development provided me with in-depth knowledge and practical skills in programming languages such as Python, Java, and C++. Through courses like Advanced Programming, Data Structures and Algorithms, Web Development, and Database Management Systems, I gained a comprehensive understanding of both theoretical concepts and their real-world applications."',
+			'During my time at Oulu University of Applied Sciences (Oulun Ammattikorkeakoulu), where I earned a Bachelor of Information Technology, I developed a strong foundation in software development. My specialization in software development provided me with in-depth knowledge and practical skills in programming languages such as Python, Java, and C++. Through courses like Advanced Programming, Data Structures and Algorithms, Web Development, and Database Management Systems, I gained a comprehensive understanding of both theoretical concepts and their real-world applications.',
 		link: "#",
 		year: "2018"
 	},
 	{
 		id: 2,
 		image: blog,
-		title: "Second blog",
+		title: "Kärsämäen lukio",
 		text:
-			'"No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful..."',
+			'"During my high school studies at Kärsämäen lukio, I developed a solid foundation in STEM subjects, particularly in mathematics and computer science. Courses in advanced mathematics and introductory programming sparked my interest in technology and problem-solving. I participated in various science fairs, where I presented projects that integrated both theoretical knowledge and practical application, enhancing my research and presentation skills.',
 		link: "#",
 		date: "12",
 		month: "June",
@@ -24,9 +24,9 @@ const blogs = [
 	{
 		id: 3,
 		image: blog,
-		title: "Third blog",
+		title: "Work and Code: Full Stack/Valmennuskeskus",
 		text:
-			'"Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure..."',
+			'Work and Code: Full Stack -koulutusohjelma" course, I gained comprehensive skills in full stack development, focusing on React.js. The program covered front-end and back-end technologies, enabling me to build complete web applications. I learned advanced React.js concepts such as component architecture and state management. Additionally, I studied server-side development using Node.js and Express.js, along with database management with MongoDB. Through projects, I gained practical experience and improved teamwork skills, equipping me to tackle web development challenges',
 		link: "#",
 		date: "24",
 		month: "july",
@@ -35,9 +35,9 @@ const blogs = [
 	{
 		id: 4,
 		image: blog,
-		title: "Fourth blog",
+		title: "Data-analysis/Opiframe",
 		text:
-			'"To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?" - "de Finibus Bonorum et Malorum" by Cicero in 45 BC',
+			'During a data analysis course at Opiframe, I gained essential skills in Power BI, Google Analytics, and Python libraries like Matplotlib, Numpy, and Seaborn. Through practical projects, I learned to analyze complex datasets, visualize data effectively, and derive actionable insights. This training prepared me to handle diverse data-related challenges in professional settings.',
 		link: "#",
 		date: "8",
 		month: "August",
@@ -46,9 +46,9 @@ const blogs = [
 	{
 		id: 5,
 		image: blog,
-		title: "Fifth blog",
+		title: "ITC-osaaja/Opiframe",
 		text:
-			'"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain..."',
+			'During my ITC-osaaja course, I acquired valuable skills in various tools and methodologies essential for IT professionals. The course covered a range of topics including Office 365, Github, SourceTree, and Scrum. In Office 365, I learned to utilize its suite of applications for productivity and collaboration, enhancing my ability to work efficiently in a professional environment. Github and SourceTree training provided me with practical experience in version control and collaboration workflows, essential for software development projects. Moreover, I gained insight into Scrum methodology, learning how to effectively manage projects in an Agile environment. This included understanding Scrum principles, roles, ceremonies, and artifacts, enabling me to contribute effectively to Agile teams and projects.',
 		link: "#",
 		date: "21",
 		month: "September",
@@ -57,14 +57,25 @@ const blogs = [
 	{
 		id: 6,
 		image: blog,
-		title: "Sixth blog",
+		title: "Kaleva Media",
 		text:
-			'"These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."  - "de Finibus Bonorum et Malorum" by Cicero in 45 BC',
+			'During my time at Kaleva Media, I conducted data analysis projects to understand user engagement and content consumption. Using tools like Google Analytics, I tracked website traffic and user demographics, providing insights for optimizing content delivery and user experience. Collaborating with various teams, I interpreted data findings to inform content strategy and marketing campaigns, honing my skills in media analytics and decision-making.',
 		link: "#",
 		date: "16",
 		month: "November",
 		year: "2020"
-	}
+	},
+	{
+		id: 6,
+		image: blog,
+		title: "Lucci Oy",
+		text:
+			'While my employment at Lucci Oy, my primary responsibility was the implementation of WordPress websites based on designs provided by the design team or clients. Working closely with designers, I translated their design mockups into functional websites using WordPress as the content management system (CMS).\n\nMy role involved setting up WordPress installations, configuring themes, and customizing layouts to match the provided designs. I also integrated necessary plugins and functionalities to ensure the websites met the desired specifications and requirements.\n\nThroughout the process, I maintained clear communication with designers and clients to address any questions or concerns and ensure that the final website accurately reflected the intended design and met the client\'s expectations.\n\nMy experience at Lucci Oy allowed me to refine my skills in WordPress development and collaboration with design teams, enabling me to efficiently bring design concepts to life in the form of fully functional websites.',
+		link: "#",
+		date: "16",
+		month: "November",
+		year: "2020"
+	},
 ];
 
 export default blogs;
