fa233058531507d57dca5c8def3b9d14c7a843ac
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2024-06-07 17:50:22 +0300

    change block to education and add text

--- src/data/allBlogs.js
+++ src/data/allBlogs.js
@@ -4,10 +4,10 @@ const blogs = [
 	{
 		id: 1,
 		image: blog,
-		title: "First blog",
+		title: "Engineer(OAMK) Information technology",
 		text:
-			'Wise words by Cicero: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness..."',
-		link: "https://www.facebook.com/",
+			'During my time at Oulun Ammattikorkeakoulu (Oulu University of Applied Sciences), where I earned a Bachelor of Information Technology, I developed a strong foundation in software development. My specialization in software development provided me with in-depth knowledge and practical skills in programming languages such as Python, Java, and C++. Through courses like Advanced Programming, Data Structures and Algorithms, Web Development, and Database Management Systems, I gained a comprehensive understanding of both theoretical concepts and their real-world applications."',
+		link: "#",
 		date: "8",
 		month: "March",
 		year: "2021"
@@ -18,7 +18,7 @@ const blogs = [
 		title: "Second blog",
 		text:
 			'"No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful..."',
-		link: "https://www.facebook.com/",
+		link: "#",
 		date: "12",
 		month: "June",
 		year: "2021"
@@ -29,7 +29,7 @@ const blogs = [
 		title: "Third blog",
 		text:
 			'"Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure..."',
-		link: "https://www.facebook.com/",
+		link: "#",
 		date: "24",
 		month: "july",
 		year: "2019"
@@ -40,7 +40,7 @@ const blogs = [
 		title: "Fourth blog",
 		text:
 			'"To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?" - "de Finibus Bonorum et Malorum" by Cicero in 45 BC',
-		link: "https://www.facebook.com/",
+		link: "#",
 		date: "8",
 		month: "August",
 		year: "2020"
@@ -51,7 +51,7 @@ const blogs = [
 		title: "Fifth blog",
 		text:
 			'"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain..."',
-		link: "https://www.facebook.com/",
+		link: "#",
 		date: "21",
 		month: "September",
 		year: "2020"
@@ -62,7 +62,7 @@ const blogs = [
 		title: "Sixth blog",
 		text:
 			'"These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."  - "de Finibus Bonorum et Malorum" by Cicero in 45 BC',
-		link: "https://www.facebook.com/",
+		link: "#",
 		date: "16",
 		month: "November",
 		year: "2020"
