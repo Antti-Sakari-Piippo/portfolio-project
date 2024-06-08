d6be244cbe8b28662a73b5c46a1b880d32830280
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2024-06-08 20:46:29 +0300

    update data

--- src/data/allBlogs.js
+++ src/data/allBlogs.js
@@ -24,8 +24,6 @@ const blogs = [
 		text:
 			'During my high school studies at Kärsämäki high school, I developed a solid foundation in STEM subjects, particularly in mathematics and computer science. Courses in advanced mathematics and introductory programming sparked my interest in technology and problem-solving. I participated in various science fairs, where I presented projects that integrated both theoretical knowledge and practical application, enhancing my research and presentation skills.',
 		link: "#",
-		date: "12",
-		month: "June",
 		year: "2009-2012"
 	},
 	{
@@ -35,9 +33,7 @@ const blogs = [
 		text:
 			'Taking part in "Work and Code: Full Stack" -course, I gained skills in full stack development, focusing on React.js. The program covered front-end and back-end technologies, enabling me to build complete web applications. I learned advanced React.js concepts such as component architecture and state management. Additionally, I studied server-side development using Node.js and Express.js, along with database management with MongoDB. Through projects, I gained practical experience and improved teamwork skills, equipping me to tackle web development challenges',
 		link: "#",
-		date: "24",
-		month: "july",
-		year: "2019"
+		year: "2022"
 	},
 	{
 		id: 4,
@@ -46,20 +42,16 @@ const blogs = [
 		text:
 			'During a data analysis course at Opiframe, I gained essential skills in Power BI, Google Analytics, and Python libraries like Matplotlib, Numpy, and Seaborn. Through practical projects, I learned to analyze complex datasets, visualize data effectively, and derive actionable insights. This training prepared me to handle diverse data-related challenges in professional settings.',
 		link: "#",
-		date: "8",
-		month: "August",
-		year: "2020"
+		year: "2021"
 	},
 	{
 		id: 5,
 		image: kaleva,
 		title: "Kaleva Media Oy",
 		text:
-			'During my time at Kaleva Media, I conducted data analysis projects to understand user engagement and content consumption. Using tools like Google Analytics, I tracked website traffic and user demographics, providing insights for optimizing content delivery and user experience. Collaborating with various teams, I interpreted data findings to inform content strategy and marketing campaigns, honing my skills in media analytics and decision-making.',
+			'During my time at Kaleva Media, I conducted data analysis projects to understand user engagement and content consumption. Using tools like Snowflake, Tableau, python, I tracked website traffic and user demographics, providing insights for optimizing content delivery and user experience. Collaborating with various teams, I interpreted data findings to inform content strategy and marketing campaigns, honing my skills in media analytics and decision-making.',
 		link: "#",
-		date: "16",
-		month: "November",
-		year: "2022"
+		year: "2022-2023"
 	},
 	{
 		id: 6,
@@ -68,9 +60,7 @@ const blogs = [
 		text:
 			'While my employment at Lucci Oy, my primary responsibility was the implementation of WordPress websites based on designs provided by the design team or clients. Working closely with designers, I translated their design mockups into functional websites using WordPress as the content management system (CMS).\n\nMy role involved setting up WordPress installations, configuring themes, and customizing layouts to match the provided designs. I also integrated necessary plugins and functionalities to ensure the websites met the desired specifications and requirements.\n\nThroughout the process, I maintained clear communication with designers and clients to address any questions or concerns and ensure that the final website accurately reflected the intended design and met the client\'s expectations.\n\nMy experience at Lucci Oy allowed me to refine my skills in WordPress development and collaboration with design teams, enabling me to efficiently bring design concepts to life in the form of fully functional websites.',
 		link: "#",
-		date: "16",
-		month: "November",
-		year: "2020"
+		year: "2019-2020"
 	},
 ];
 
