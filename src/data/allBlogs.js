5625a296bbf10770bb15db55e7043a6ac17a80c9
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2024-06-08 18:35:55 +0300

    add images to work/education section

--- src/data/allBlogs.js
+++ src/data/allBlogs.js
@@ -1,9 +1,16 @@
 import blog from "../images/blogs/defaultBlog.png";
+import kaleva from "../images/blogs/kalevamedia.png";
+import oamk from "../images/blogs/OAMK.png
+import lucci from "../images/blogs/lucci.jpg";
+import karsamaenlukio from "../images/blogs/karsamaenlukio.png";
+import buutti from "../images/blogs/buuttijpg.jpg";
+import valmennuskeskus from "../images/blogs/valmennuskeskuspng.png";
+import opiframe from "../images/blogs/opiframe.png";
 
 const blogs = [
 	{
 		id: 1,
-		image: blog,
+		image: oamk,
 		title: "OAMK",
 		text:
 			'During my time at Oulu University of Applied Sciences (Oulun Ammattikorkeakoulu), where I earned a Bachelor of Information Technology, I developed a strong foundation in software development. My specialization in software development provided me with in-depth knowledge and practical skills in programming languages such as Python, Java, and C++. Through courses like Advanced Programming, Data Structures and Algorithms, Web Development, and Database Management Systems, I gained a comprehensive understanding of both theoretical concepts and their real-world applications.',
@@ -12,7 +19,7 @@ const blogs = [
 	},
 	{
 		id: 2,
-		image: blog,
+		image: karsamaenlukio,
 		title: "Kärsämäki high school",
 		text:
 			'"During my high school studies at Kärsämäen lukio, I developed a solid foundation in STEM subjects, particularly in mathematics and computer science. Courses in advanced mathematics and introductory programming sparked my interest in technology and problem-solving. I participated in various science fairs, where I presented projects that integrated both theoretical knowledge and practical application, enhancing my research and presentation skills.',
@@ -23,7 +30,7 @@ const blogs = [
 	},
 	{
 		id: 3,
-		image: blog,
+		image: buutti,
 		title: "Work and Code/Buutti",
 		text:
 			'Work and Code: Full Stack" -course, I gained comprehensive skills in full stack development, focusing on React.js. The program covered front-end and back-end technologies, enabling me to build complete web applications. I learned advanced React.js concepts such as component architecture and state management. Additionally, I studied server-side development using Node.js and Express.js, along with database management with MongoDB. Through projects, I gained practical experience and improved teamwork skills, equipping me to tackle web development challenges',
@@ -34,7 +41,7 @@ const blogs = [
 	},
 	{
 		id: 4,
-		image: blog,
+		image: opiframe,
 		title: "Data-analysis/Opiframe",
 		text:
 			'During a data analysis course at Opiframe, I gained essential skills in Power BI, Google Analytics, and Python libraries like Matplotlib, Numpy, and Seaborn. Through practical projects, I learned to analyze complex datasets, visualize data effectively, and derive actionable insights. This training prepared me to handle diverse data-related challenges in professional settings.',
@@ -45,7 +52,7 @@ const blogs = [
 	},
 	{
 		id: 5,
-		image: blog,
+		image: opiframe,
 		title: "ITC-osaaja/Opiframe",
 		text:
 			'During my ITC-osaaja course, I acquired valuable skills in various tools and methodologies essential for IT professionals. The course covered a range of topics including Office 365, Github, SourceTree, and Scrum. In Office 365, I learned to utilize its suite of applications for productivity and collaboration, enhancing my ability to work efficiently in a professional environment. Github and SourceTree training provided me with practical experience in version control and collaboration workflows, essential for software development projects. Moreover, I gained insight into Scrum methodology, learning how to effectively manage projects in an Agile environment. This included understanding Scrum principles, roles, ceremonies, and artifacts, enabling me to contribute effectively to Agile teams and projects.',
@@ -56,7 +63,7 @@ const blogs = [
 	},
 	{
 		id: 6,
-		image: blog,
+		image: kaleva,
 		title: "Kaleva Media Oy",
 		text:
 			'During my time at Kaleva Media, I conducted data analysis projects to understand user engagement and content consumption. Using tools like Google Analytics, I tracked website traffic and user demographics, providing insights for optimizing content delivery and user experience. Collaborating with various teams, I interpreted data findings to inform content strategy and marketing campaigns, honing my skills in media analytics and decision-making.',
@@ -67,7 +74,7 @@ const blogs = [
 	},
 	{
 		id: 6,
-		image: blog,
+		image: lucci,
 		title: "Lucci Oy",
 		text:
 			'While my employment at Lucci Oy, my primary responsibility was the implementation of WordPress websites based on designs provided by the design team or clients. Working closely with designers, I translated their design mockups into functional websites using WordPress as the content management system (CMS).\n\nMy role involved setting up WordPress installations, configuring themes, and customizing layouts to match the provided designs. I also integrated necessary plugins and functionalities to ensure the websites met the desired specifications and requirements.\n\nThroughout the process, I maintained clear communication with designers and clients to address any questions or concerns and ensure that the final website accurately reflected the intended design and met the client\'s expectations.\n\nMy experience at Lucci Oy allowed me to refine my skills in WordPress development and collaboration with design teams, enabling me to efficiently bring design concepts to life in the form of fully functional websites.',
