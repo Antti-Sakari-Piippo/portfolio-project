57771415c904f92860234020e2a7aa7cc0c8910b
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2024-06-09 12:24:51 +0300

    change svg to jpg

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -20,8 +20,7 @@ export const Blog = () => {
 					</p>
 					<br />
 					<Link to={`/blogs/`} className="button blog-link">
-						<img src="/images/arrow.svg" alt="back arrow" />
-						<img src="/src/images/arrow.svg" alt="back arrow" />
+						<img src="/src/images/arrow.jpg" alt="back arrow" />
 						Back
 					</Link>
 				</div>
