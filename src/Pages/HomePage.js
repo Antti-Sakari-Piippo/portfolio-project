4c1bcc237ad6f1dccb91d1d27ea69292600fbc85
Antti-Sakari-Piippo <sakarisakari11@hotmail.com>
2024-06-10 11:51:09 +0300

    add text about me

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -17,7 +17,7 @@ function HomePage() {
 					/>
 				</h1>
 				<p className="h-sub-text">
-					I'm enthusiastic ja dedicated Front-End Web Developer with a strong foundation in HTML, CSS, and JavaScript. I also have knowledge about WordPress, React.js and Next.js. My expertice extends to data-analysis, with proficiency in tools such as Power BI, Google Analytics ajd Python libraries like Matplotlib, Numby, and Seaborn. I like working in a group and independently. My passion for web development and design, coupled with hands-on experience from personal projects, drives my continuos growth in the field.
+					I'm enthusiastic ja dedicated Front-End Web Developer with a strong foundation in HTML, CSS, and JavaScript. I also have knowledge about WordPress, React.js and Next.js. My expertice extends to data-analysis, with proficiency in tools such as Power BI, Google Analytics ajd Python libraries like Matplotlib, Numby, and Seaborn. I like working in a group and independently. My passion for web development and design, coupled with hands-on experience from personal projects, drives my continuos growth in the field. Asside from my experience in tech, I am a lumberjack and I have done accounting for a company using FabricAi and Procounter.
 				</p>
 				<div className="icons">
 					<a
